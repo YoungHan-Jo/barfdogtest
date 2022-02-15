d29a1e6d9e5dff91e5210c8ece7e0961b82362ed
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2022-02-15 19:10:33 +0900

    order repository

--- src/main/java/com/bi/barfdogtest/domain/OrderItem.java
+++ src/main/java/com/bi/barfdogtest/domain/OrderItem.java
@@ -28,4 +28,20 @@ public class OrderItem {
     private int orderPrice; // 주문가격
 
     private int count; // 주문 수량
+
+    // == 비즈니스 로직 ==/
+    /*
+    * 상품 재고 정상으로 돌리기
+    * */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // == 조회 로직 == //
+    /*
+    * 주문 상품 전체 가격 조회
+    * */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
