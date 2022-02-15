66092ee7109ca7230d718126cc5ab43af5a77d1c
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2022-02-15 09:46:21 +0900

    commit

--- src/main/java/com/bi/barfdogtest/domain/Order.java
+++ src/main/java/com/bi/barfdogtest/domain/Order.java
@@ -7,6 +7,8 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 @Table(name = "orders") // 테이블 이름 수동 설정
@@ -16,14 +18,14 @@ public class Order {
     @Column(name = "order_id") // 컬럼명 수동 설정
     private Long id;
 
-    @ManyToOne // 다 대 일 관계
+    @ManyToOne(fetch = LAZY) // 다 대 일 관계
     @JoinColumn(name = "member_id") // FK 설정 // 연관관계의 주인
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems;
 
-    @OneToOne // 일 대 일 관계 // FK를 어디둘지 정해야함.
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL) // 일 대 일 관계 // FK를 어디둘지 정해야함.
     @JoinColumn(name = "delivery_id") // 엑세스가 더 높은곳을 연관관계의 주인으로 설정
     private Delivery delivery;
 
@@ -32,4 +34,18 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // [ORDER, CANCEL]
 
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
---
+++ src/test/java/com/bi/barfdogtest/service/MemberServiceTest.java
@@ -0,0 +1,17 @@
+package com.bi.barfdogtest.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+    
+
+}
