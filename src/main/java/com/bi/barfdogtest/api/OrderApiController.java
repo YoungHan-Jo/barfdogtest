78b6aeb981b1b657e992f07d56d6dc9312fed43e
JYH <verin4494@gmail.com>
2022-03-01 12:46:16 +0900

    v6

--- src/main/java/com/bi/barfdogtest/api/OrderApiController.java
+++ src/main/java/com/bi/barfdogtest/api/OrderApiController.java
@@ -2,6 +2,8 @@ package com.bi.barfdogtest.api;
 
 import com.bi.barfdogtest.domain.*;
 import com.bi.barfdogtest.repository.OrderRepository;
+import com.bi.barfdogtest.repository.order.query.OrderFlatDto;
+import com.bi.barfdogtest.repository.order.query.OrderItemQueryDto;
 import com.bi.barfdogtest.repository.order.query.OrderQueryDto;
 import com.bi.barfdogtest.repository.order.query.OrderQueryRepository;
 import lombok.AllArgsConstructor;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -60,6 +65,24 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public Result ordersV5() {
         List<OrderQueryDto> orders = orderQueryRepository.findAllByDto_optimization();
+        return new Result(orders);
+    }
+
+    @GetMapping("/api/v6/orders")
+    public Result ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        List<OrderQueryDto> orders = flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
+
         return new Result(orders);
     }
 
